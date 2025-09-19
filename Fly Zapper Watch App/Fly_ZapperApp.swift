e54ae8420429a110d532ab7e3dfe8464c7523415
Emilio Montes <rmontesl@hotmail.com>
2025-09-18 19:35:26 -0700

    Initial Commit

---
+++ Fly Zapper Watch App/Fly_ZapperApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fly_ZapperApp.swift
+//  Fly Zapper Watch App
+//
+//  Created by Renaud Montes on 9/18/25.
+//
+
+import SwiftUI
+
+@main
+struct Fly_Zapper_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
